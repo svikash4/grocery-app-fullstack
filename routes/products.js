a0cc39edbbcaa17c7543b5f6b07e6c46c2e72f7d
svikash4 <vikash.sharma749@gmail.com>
2025-04-05 15:28:49 +0530

    Adding files

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const sql = require('mssql');
-const dbConfig = require('../dbconfig');
+const { database } = require('../dbconfig');
+
+const products = database.container('Products');
 
 router.get('/products', async (req, res) => {
   try {
-    await sql.connect(dbConfig);
-    const result = await sql.query`SELECT * FROM Products`;
-    res.json(result.recordset);
+    const { resources } = await products.items.query("SELECT * FROM c").fetchAll();
+    res.json(resources);
   } catch (err) {
     res.status(500).send(err.message);
   }
